50c2e5e639a9b9209486546f828d6a8b7c6e35ed
F.R. van der Meulen <0923835@hr.nl>
2018-06-15 17:29:19 +0200

    Template final assignment

--- Assignment Three/drawLine.py
+++ Assignment Three/drawLine.py
@@ -2,8 +2,6 @@
 from grid import *
 from random import randint
 
-debug = True	#Boolean for debug mode
-
 g = Grid(50, 50)	#Grid with tiles
 
 #Coordinates of two points
@@ -11,11 +9,6 @@ x1 = randint(0, 49)
 y1 = randint(0, 49)
 x2 = randint(0, 49)
 y2 = randint(0, 49)
-
-if debug:
-	#Print x's and y's of points
-	print("Point 1: X:", x1, "Y:", y1)
-	print("Point 2: X:", x2, "Y:", y2)
 
 #Methods
 def derivativeY(hor1, ver1, hor2, ver2):
@@ -78,11 +71,6 @@ if x1 > x2:
 			tempY += c2
 			g.addPoint(tempX, tempY)
 
-		if debug:
-			print("Point 1 is below and to the right of point 2")
-			print("Y coefficient:", c1)
-			print("X coefficient:", c2)
-
 	elif y2 > y1:
 		#Point 1 is above and to the right of point 2
 		#Coefficient is negative, line goes from 2 to 1
@@ -105,18 +93,9 @@ if x1 > x2:
 			tempY += c2
 			g.addPoint(tempX, tempY)
 
-		if debug:
-			print("Point 1 is above and to the right of point 2")
-			print("Y coefficient:", c1)
-			print("X coefficient:", c2)
-
 	elif y1 == y2:
 		#Point 1 is to the right of point 2
 		horizontalLine(x2, x1, y1)
-
-		if debug:
-			print("Point 1 is to the right of point 2")
-			print("Line is straight horizontal")
 
 elif x2 > x1:
 	if y1 > y2:
@@ -141,11 +120,6 @@ elif x2 > x1:
 			tempY += c2
 			g.addPoint(tempX, tempY)
 
-		if debug:
-			print("Point 1 is below and to the left of point 2")
-			print("Y coefficient:", c1)
-			print("X coefficient:", c2)
-
 	elif y2 > y1:
 		#Point 1 is above and to the left of point 2
 		#Coefficient is positive, line goes from 1 to 2
@@ -168,35 +142,18 @@ elif x2 > x1:
 			tempY += c2
 			g.addPoint(tempX, tempY)
 
-		if debug:
-			print("Point 1 is above and to the left of point 2")
-			print("Y coefficient:", c1)
-			print("X coefficient:", c2)
-
 	elif y1 == y2:
 		#Point 1 is to the left of point 2
 		horizontalLine(x1, x2, y1)
-
-		if debug:
-			print("Point 1 is to the left of point 2")
-			print("Line is straight horizontal")
 
 elif x1 == x2:
 	if y1 > y2:
 		#Point 1 is below point 2
 		verticalLine(x1, y2, y1)
 
-		if debug:
-			print("Point 1 is below point 2")
-			print("Line is straight vertical")
-
 	elif y2 > y1:
 		#Point 1 is above point 2
 		verticalLine(x1, y1, y2)
-
-		if debug:
-			print("Point 1 is above point 2")
-			print("Line is straight vertical")
 
 	elif y1 == y2:
 		#Point 1 is point 2
